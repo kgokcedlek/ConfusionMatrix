9ecfbf61493056f29c8bf0843ab9879b3037c2f7
Gokce Kisbet <kgokcedlek@gmail.com>
2021-01-03 03:37:57 +0300

    route

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Start an app route which is '/'
-@app.route('/')
+@app.route('https://confusionmatrix.herokuapp.com/')
 #Declare the main function
 def main():
     return render_template('app.html')
